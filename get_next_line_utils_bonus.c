5cb54345aa4f17cd3cb553a1e398ae490ee17bdf
Abdelouahab Kemmoune <akemmoun@c4r6p7.1337.ma>
2024-12-01 15:09:02 +0100

    first and last commit

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akemmoun <akemmoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 10:15:43 by akemmoun          #+#    #+#             */
+/*   Updated: 2024/12/01 10:36:33 by akemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	size_t		i;
+	char		cc;
+
+	cc = (char) c;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == cc)
+			return ((char *) &str[i]);
+		i++;
+	}
+	if (cc == '\0')
+		return ((char *) &str[i]);
+	return (NULL);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dest;
+	size_t	i;
+
+	i = 0;
+	dest = malloc (ft_strlen(str) + 1);
+	if (!dest)
+		return (NULL);
+	while (str[i])
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = malloc (len + 1);
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[len] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	str = malloc ((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!str)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (j < ft_strlen(s2))
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
